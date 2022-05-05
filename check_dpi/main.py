7fd07bab7f2b63dc4892d2dccbb7bdffd2be4232
Aleksey Voko <voko.aleksey@gmail.com>
2022-05-05 08:45:26 +0400

    fix(check_dpi): fix 'main.py' - add Pillow lib

--- check_dpi/main.py
+++ check_dpi/main.py
@@ -1,7 +1,20 @@
-def main():
-    input('Задать минимальный высокий DPI: ')
+from pathlib import Path
 
-    print('Сортировка завершена')
+from PIL import Image
+
+
+def main():
+    # MIN_DPI = input('Задать минимальный высокий DPI: ')
+
+    images = Path().rglob('*.jpg')
+    for image in images:
+        im = Image.open(image)
+        print(image)
+        print(im.format, im.size, im.mode)
+        print(im.info)
+        print()
+
+    # print('Сортировка завершена')
     print('Для выхода нажмите Enter')
     input()
 
